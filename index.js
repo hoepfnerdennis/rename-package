6dd0255e9e96f05eeffd41fa576d3a896f713872
Dennis Höpfner <dennis.hoepfner@dm.de>
2022-11-23 00:21:55 +0100

    v1.0.1

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 #!/usr/bin/env node
-const fs = require("fs");
-const path = require("path");
-const packageJson = require(path.resolve(process.cwd(), "package.json"));
-const newName = process.argv[process.argv.length - 1];
-if (newName.endsWith("rename-package")) {
+const FS = require("fs");
+const PATH = require("path");
+
+const name = process.argv
+  .find((arg) => arg.startsWith("--name="))
+  ?.replace("--name=", "");
+const path = process.argv
+  .find((arg) => arg.startsWith("--path="))
+  ?.replace("--path=", "");
+
+if (!name) {
   console.error("No new name found.");
   return process.exit(1);
 }
-console.log('Current name is "' + packageJson.name + '"');
-console.log('New name will be "' + newName + '"');
-packageJson.name = newName;
 
-fs.writeFileSync(
-  path.resolve(process.cwd(), "package.json"),
+if (name.endsWith("rename-package")) {
+  console.error("No new name found.");
+  return process.exit(1);
+}
+
+const packageJson = require(PATH.resolve(process.cwd(), path, "package.json"));
+
+console.log('Current name is "' + packageJson.name + '"');
+console.log('New name will be "' + name + '"');
+packageJson.name = name;
+
+FS.writeFileSync(
+  PATH.resolve(process.cwd(), "package.json"),
   JSON.stringify(packageJson, null, 2)
 );
